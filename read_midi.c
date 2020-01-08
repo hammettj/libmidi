52a520a2eb0b2c7b1144832ff9fd3666cb5676de
Jeffrey <920597-jhammett@users.noreply.gitlab.com>
2020-01-08 20:38:23 +0100

    read midi header fields
    currently only midi files of type 1 and metrical timing are supported

--- read_midi.c
+++ read_midi.c
@@ -6,6 +6,12 @@ int main(int argc, char** argv) {
 	midi* midi = NULL;
 	if ((midi = midi_open("super-mario.mid"))) {
 		printf("MIDI file opened!\n");
+
+		printf("format: %u\n", midi->header.format);
+		printf("ntracks: %u\n", midi->header.ntracks);
+		printf("timecode: %u\n", midi->header.timecode);
+		printf("ppqn: %u\n", midi->header.ppqn);
+
 		if (midi_close(midi)) {
 			printf("MIDI file closed!\n");
 		} else {
--- src/midi.c
+++ src/midi.c
@@ -10,11 +10,33 @@ midi* midi_open(const char* filename) {
 
 	// parse midi header
 	if (read_uint8t(file) != 'M' || read_uint8t(file) != 'T' || read_uint8t(file) != 'h' || read_uint8t(file) != 'd') {
+		mem_file_close(file);
+		return NULL;
+	}
+
+	uint32_t len = read_uint32t(file);
+	if (len != 6) { // length should be 6 bytes
+		mem_file_close(file);
 		return NULL;
 	}
 
 	midi* midi = malloc(sizeof(midi));
 	midi->file = file;
+	midi->header.format = read_uint16t(file);
+	midi->header.ntracks = read_uint16t(file);
+
+	uint16_t tickdiv = read_uint16t(file);
+
+	if (tickdiv & (1 << 15)) {
+		// fixme not supported yet..
+		mem_file_close(file);
+		free(midi);
+
+		return NULL;
+	} else {
+		midi->header.timecode = 0;
+		midi->header.ppqn = tickdiv >> 1;
+	}
 
 	return midi;
 }
